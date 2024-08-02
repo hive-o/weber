41510c1afebdfcd00b15ebcabdef29142948d928
Open Cipher <8209379+open-cipher@users.noreply.github.com>
2024-08-02 21:57:46 +0530

    feat: export puppeteer objects

--- src/browser.ts
+++ src/browser.ts
@@ -1,21 +1,21 @@
 import { BrowserContext, launch, Browser as PuppeteerBrowser } from 'puppeteer';
 
-export class Browser {
+export class WeberBrowser {
   private _context?: BrowserContext; // Singleton instance
 
-  private static _instance: Browser;
+  private static _instance: WeberBrowser;
 
   private browser?: PuppeteerBrowser;
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {}
 
-  public static instance(): Browser {
-    if (!Browser._instance) {
-      Browser._instance = new Browser();
+  public static instance(): WeberBrowser {
+    if (!WeberBrowser._instance) {
+      WeberBrowser._instance = new WeberBrowser();
     }
 
-    return Browser._instance;
+    return WeberBrowser._instance;
   }
 
   public async close(): Promise<void> {
